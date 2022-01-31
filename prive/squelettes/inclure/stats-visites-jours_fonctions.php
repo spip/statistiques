30071319aa12569a61da73a15997422b62f51f4c
Cerdic <cedric@yterium.com>
2022-01-31 15:46:12 +0100

    Utiliser l'API transmettre.api pour les acces aux stats json et csv

--- prive/squelettes/inclure/stats-visites-jours_fonctions.php
+++ prive/squelettes/inclure/stats-visites-jours_fonctions.php
@@ -11,21 +11,28 @@ include_spip('prive/squelettes/inclure/stats-visites-data_fonctions');
  *
  * Nécessite une autorisation avec l'auteur connecté sur l'url cible.
  */
-function statistiques_url_data(array $Pile, string $output = 'json', string $export = 'visites', string $unite = 'jour', ?int $duree = null): string {
-	$url = generer_url_public("statistiques.$output");
-	$url = parametre_url($url, 'export', $export);
-	$url = parametre_url($url, 'unite', $unite);
+function statistiques_url_data(array $Pile, string $output = 'json', string $export = 'visites', string $unite = 'jour', ?int $duree = null, ?bool $public = null): string {
+	include_spip('inc/acces');
+
+	$args = [
+		'export' => $export,
+		'unite' => $unite,
+	];
+
 	if ($duree and $duree > 0) {
-		$url = parametre_url($url, 'duree', $duree);
+		$args['duree'] = $duree;
 	}
 	$objet = $Pile[0]['objet'] ?? null;
 	$id_objet = $Pile[0]['id_objet'] ?? null;
 	if ($objet) {
-		$url = parametre_url($url, 'objet', $objet);
+		$args['objet'] = $objet;
 	}
 	if ($id_objet) {
-		$url = parametre_url($url, 'id_objet', $id_objet);
+		$args['id_objet'] = $id_objet;
 	}
+
+	$args = http_build_query($args);
+	$url = generer_url_api_low_sec('transmettre', $output, 'statistiques.' . $output, '', $args, false, $public);
 	return $url;
 }
 
@@ -36,8 +43,6 @@ function statistiques_url_data(array $Pile, string $output = 'json', string $exp
  * Possiblement utilisé pour télécharger périodiquement ses statistiques depuis un cron
  */
 function statistiques_url_data_auteur(array $Pile, string $output = 'json', string $export = 'visites', string $unite = 'jour', ?int $duree = null): string {
-	$url = statistiques_url_data($Pile, $output, $export, $unite, $duree);
-	$params = param_low_sec('statistiques', ['hash' => md5($url)], '', 'voirstats');
-	// pas besoin de l'arg hash. Il sert juste à calculer une clé unique pour l'auteur.
-	return parametre_url($url . '&' . $params, 'hash', '');
+	$url = statistiques_url_data($Pile, $output, $export, $unite, $duree, true);
+	return $url;
 }
